0e96155e911b1e2c9164f1eced829ea4aad408ee
elseManoj <manoj.kp20150523@gmail.com>
2024-02-08 23:14:01 +0530

    Adding session for the user

--- app/controllers/regestrations_controller.rb
+++ app/controllers/regestrations_controller.rb
@@ -5,7 +5,8 @@ class RegestrationsController < ApplicationController
 
     def create        
         @user = User.new(user_params)        
-        if @user.save 
+        if @user.save             
+            session[:user_id] = @user.id
             redirect_to root_path, notice: "Successfully logged in"
         else
             render :new
